8286e1cadee402865660f6a55c4bbdc13277195a
Matias <matiasba_fb@hotmail.com>
2023-02-21 23:28:47 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const obtenerFrase = () => {
+    axios.get("https://thesimpsonsquoteapi.glitch.me/quotes")
+      .then(response => {
+        setFrase(response.data)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+  const [frase, setFrase] = useState(null)
+
+  if(frase === null){
+    return(
+      <>
+        <div id='bienvenida'>
+          <h1 id='titulo'>The Simpsons</h1>
+          <h3 id="arrow">Click for quotes ⬇</h3>
+          <button onClick={obtenerFrase}>Let's go!</button>
+        </div>
+      </>
+    )
+  }else{
+    return (
+      <>
+        <div id='contenedor-quote'>
+          <div id='quote'>
+            <h1 id='titulo'>The Simpsons</h1>
+            <div id='imagen'>
+              <img src={frase[0].image} height="220px" alt='character photo'/>
+            </div>
+            <p id='contenido-frase'>{frase[0].quote}</p>
+            <p id='autor'>{frase[0].character}</p>
+            <div id='contenedor-boton'>
+              <button onClick={obtenerFrase}>Next</button>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
